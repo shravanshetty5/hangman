2184e6be36b255692332f0f4d3fa9e20d71ac703
Shravan Shetty <shravash@cisco.com>
2020-08-27 18:19:17 -0500

    added hint text, fixed bug with game continuing, and added correct word to the end modal

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const wordContainer = document.getElementById('word');
+const hintContainer = document.getElementById('hint');
 const wrongLettersContainer = document.getElementById('wrong-letters');
 const playAgainBtn = document.getElementById('play-button');
 const popUp = document.getElementById('popup-container');
@@ -6,7 +7,10 @@ const notification = document.getElementById('notification-container');
 const finalMsg = document.getElementById('final-message');
 const figureParts = document.querySelectorAll('.figure-part');
 
-let selectedWord, correctLetters, wrongLetters;
+let selectedWord,
+  correctLetters,
+  wrongLetters,
+  hintText = 'Hint: Dictonary did not have a hint. Sorry';
 //display word
 
 function displayWord() {
@@ -26,7 +30,6 @@ function displayWord() {
     finalMsg.innerText = 'Congratulations! You won!!';
     popUp.style.display = 'flex';
   }
-  console.log(innerWord);
 }
 
 function displayWrongWords() {
@@ -39,11 +42,13 @@ function displayWrongWords() {
 function updateHangman() {
   const errors = wrongLetters.length;
   if (errors === figureParts.length) {
-    finalMsg.innerText = 'Opps, you lost. Try again';
+    finalMsg.innerText = `
+    Opps, you lost. The word was
+         "${selectedWord}"
+    Try again`;
     popUp.style.display = 'flex';
   }
   figureParts.forEach((part, index) => {
-    console.log('updating');
     if (index < errors) {
       part.classList.add('display');
     } else {
@@ -66,6 +71,30 @@ function initializeGame() {
       displayWord();
       //updateHangmaan
       updateHangman();
+      //clearHint
+      //getDefinition for hint
+      getDefinition(selectedWord);
+    });
+}
+
+function getDefinition(word) {
+  fetch(
+    `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${word}`,
+    {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'mashape-community-urban-dictionary.p.rapidapi.com',
+        'x-rapidapi-key': 'f86afcb4f7mshc0893fcc0f5c1bap172d51jsnb8100c146d08',
+      },
+    }
+  )
+    .then((resp) => resp.json())
+    .then((resp) => {
+      console.log(resp);
+      if (resp && resp.list[0] && resp.list[0].definition) {
+        console.log('passes');
+        hintText = 'Hint: ' + resp.list[0].definition;
+      }
     });
 }
 
@@ -76,12 +105,24 @@ function displayNotification() {
   }, 2000);
 }
 
+function displayHint() {
+  //display hint on last try
+  if (wrongLetters.length === figureParts.length - 1) {
+    hintContainer.innerText = `${hintText}`;
+  } else {
+    hintContainer.innerText = '';
+  }
+}
+
 initializeGame();
 
 //add event listner to listen to keydown events
-
 window.addEventListener('keydown', (e) => {
-  if (e.keyCode >= 65 && e.keyCode <= 90) {
+  if (
+    e.keyCode >= 65 &&
+    e.keyCode <= 90 &&
+    wrongLetters.length < figureParts.length
+  ) {
     if (selectedWord.includes(e.key)) {
       if (correctLetters.includes(e.key)) {
         displayNotification();
@@ -96,6 +137,9 @@ window.addEventListener('keydown', (e) => {
         wrongLetters.push(e.key);
         //display wrong words
         displayWrongWords();
+
+        //displayHint
+        displayHint();
         //update hangman
         updateHangman();
       }
